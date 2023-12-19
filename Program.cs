ec72550b9b2e4c2c64a29e477bdcf78a7bbce5d3
Magamed2009 <muhammadmkr6@gmail.com>
2023-12-19 15:57:52 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// string a,b;
+// int c;
+// System.Console.Write("Enter your FirstName : ");
+// a=Console.ReadLine();
+// System.Console.Write("Enter your LastName : ");
+// b=Console.ReadLine();
+// System.Console.Write("Enter your Age : ");
+// c=Convert.ToInt32(Console.ReadLine());
+// Person person = new Person();
+// person.FirstName = a; 
+// person.LastName = b;
+// person.Age = c;
+
+// System.Console.WriteLine(person.Info());
+
+// class Person
+// {
+//     public string FirstName;
+//     public string LastName;
+//     public int Age;
+//     public string Info()
+//     {
+//         return $"FirstName: {FirstName}\nLastName: {LastName}\nAge: {Age}";
+//     }
+// }
+
+// string a;
+// int b,c;
+// System.Console.Write("Text = ");
+// a=Console.ReadLine();
+// System.Console.Write("Likes : ");
+// b=Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Commets : ");
+// c=Convert.ToInt32(Console.ReadLine());
+// Person person = new Person();
+// person.FirstName = "I just updated my post"; 
+// person.LastName = b;
+// person.Age = c;
+
+// System.Console.WriteLine(person.Info());
+
+// class Person
+// {
+//     public string FirstName;
+//     public int LastName;
+//     public int Age;
+//     public string Info()
+//     {
+//         return $"Text = {FirstName}\nLikes : {LastName*2}\nCommets : {Age+1}";
+//     }
+// }
